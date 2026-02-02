9af670e60b8dad3f4fc5d038094af0b1533321fc
Ashu Kumar <ashu@example.com>
2026-02-03 01:28:30 +0530

    finally commit

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("CI/CD Ultra pro max Learning Mode 🚀");
 });
+app.get("/about",(req,res)=>{
+    res.send("This is about page")
+})
 
 app.listen(1627, () => {
   console.log("Server running on port 1627");
